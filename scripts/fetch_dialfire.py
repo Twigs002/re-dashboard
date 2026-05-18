cf423ed2f7d6c205b9f8cecb3392857bbd5e02eb
Twigs002 <paganvniekerk22@gmail.com>
2026-05-18 10:43:05 +0200

    Fix indentation issue in fetch_dialfire.py

--- scripts/fetch_dialfire.py
+++ scripts/fetch_dialfire.py
@@ -45,7 +45,7 @@ def get_current_week_bounds(now_sast):
               monday = today - datetime.timedelta(days=7)
     else:
               monday = today - datetime.timedelta(days=weekday)
-          friday = monday + datetime.timedelta(days=4)
+        friday = monday + datetime.timedelta(days=4)
     return monday, friday
 
 
